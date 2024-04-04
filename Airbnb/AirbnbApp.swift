0bc13bca42e7c5500fb45a3c5bee026216fc6e33
DanHozan123 <danhozan@gmail.com>
2024-04-04 18:09:08 +0300

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Dan Hozan on 04.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
